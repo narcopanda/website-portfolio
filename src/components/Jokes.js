b59f2d8f96373d2886431ea20f9445d1ab63ba81
sean rowland <seanrowl@buffalo.edu>
2019-05-12 19:37:46 -0400

    finished basic website

---
+++ src/components/Jokes.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+const Joke = ({joke}) => {
+  const { setup, punchline} = joke
+  return <p style={{ margin: 20}}>{setup} <em>{punchline}</em></p>
+}
+
+class Jokes extends Component {
+  state = { joke: {} , jokes: []}
+
+  componentDidMount() {
+    //fetch('https://official-joke-api.appspot.com/random_joke')
+    //.then(res => res.json())
+    //.then(json => console.log('json', json))
+    axios.get('https://official-joke-api.appspot.com/random_joke')
+      .then(res => {
+        this.setState({ joke: res.data })
+      })
+      .catch(res => alert(res.status))
+  }
+
+  getJokes = () => {
+    axios.get('https://official-joke-api.appspot.com/random_ten')
+      .then(res => {
+        this.setState({ jokes: res.data })
+      })
+      .catch(res => alert(res.message))
+  }
+
+  render(){
+    return (
+      <div>
+        <h2>Higlighted Joke</h2>
+        <Joke joke={this.state.joke}/>
+        <hr />
+      <h3>Want ten new jokes</h3>
+      <button onClick={this.getJokes}>Click me</button>
+      {
+        this.state.jokes.map(x => {
+          return <Joke key={x.id} joke={x} />
+        })
+      }
+      </div>
+    );
+  }
+}
+
+export default Jokes
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,17 @@
+import githubIcon from '../assets/github_icon.png'
+import emailIcon from '../assets/email_icon.png';
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: 'mailto:srowla94@gmail.com',
+    image: emailIcon
+  },
+  {
+    id: 2,
+    link: 'https://github.com/narcopanda',
+    image: githubIcon
+  }
+]
+
+export default SOCIAL_PROFILES
+
