18d521ba0d23dbe11e3d37c11c96dd366735109e
sean rowland <seanrowl@buffalo.edu>
2019-05-12 20:23:35 -0400

    Updates

---
+++ precache-manifest.7350e601b87399d8ccf1bd76226afcc5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6235d21bca98950545bd59b81f422f",
+    "url": "/website-portfolio/index.html"
+  },
+  {
+    "revision": "5683bfe186fad95569d0",
+    "url": "/website-portfolio/static/css/main.88eb2007.chunk.css"
+  },
+  {
+    "revision": "36ff66eefe31c321fdc2",
+    "url": "/website-portfolio/static/js/2.0346226e.chunk.js"
+  },
+  {
+    "revision": "5683bfe186fad95569d0",
+    "url": "/website-portfolio/static/js/main.d5a202ef.chunk.js"
+  },
+  {
+    "revision": "c84d5f51ad173ed9c982",
+    "url": "/website-portfolio/static/js/runtime~main.2451c7bc.js"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/website-portfolio/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/website-portfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/website-portfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "bf56edc6c7090e92662e37a9b9ed2aea",
+    "url": "/website-portfolio/static/media/project1.bf56edc6.png"
+  },
+  {
+    "revision": "f7e79c7d61c8e4a45847337bcec527e6",
+    "url": "/website-portfolio/static/media/project2.f7e79c7d.png"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/website-portfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  }
+]);
